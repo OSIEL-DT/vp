63de586d802fccda132b774093f4f70bf4a3b000
OSIEL-DT <67082238+OSIEL-DT@users.noreply.github.com>
2023-05-14 13:41:19 -0300

    Criando os metodos.

--- vp/src/main/java/vp/vp/Aluguel_produtos.java
+++ vp/src/main/java/vp/vp/Aluguel_produtos.java
@@ -1,5 +1,68 @@
 package vp.vp;
 
-public class Aluguel_produtos {
+import java.util.Objects;
 
+public class Aluguel_produtos {
+	private int cod;
+	private int data_retirada;
+	private int data_entrega;
+	private int valor;
+
+	@Override
+	public String toString() {
+		return "Aluguel_produtos [cod=" + cod + ", data_retirada=" + data_retirada + ", data_entrega=" + data_entrega
+				+ ", valor=" + valor + "]";
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+
+	public int getData_retirada() {
+		return data_retirada;
+	}
+
+	public void setData_retirada(int data_retirada) {
+		this.data_retirada = data_retirada;
+	}
+
+	public int getData_entrega() {
+		return data_entrega;
+	}
+
+	public void setData_entrega(int data_entrega) {
+		this.data_entrega = data_entrega;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cod, data_entrega, data_retirada, valor);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluguel_produtos other = (Aluguel_produtos) obj;
+		return cod == other.cod && data_entrega == other.data_entrega && data_retirada == other.data_retirada
+				&& valor == other.valor;
+	}
+
+	
 }
